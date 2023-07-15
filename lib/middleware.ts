110360843ba9f9c90f2389e8006a1a17b133bda1
glisA99 <ognjen.simic99@gmail.com>
2023-07-15 19:42:23 +0200

    added initial qp middleware

---
+++ lib/middleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+import { ParsedQs } from 'qs';
+import { parseObject } from './parsers';
+
+export function qp(request: Request, response: Response, next: NextFunction) {
+    request.query = parseObject(request.query) as ParsedQs;
+    next();
+}
